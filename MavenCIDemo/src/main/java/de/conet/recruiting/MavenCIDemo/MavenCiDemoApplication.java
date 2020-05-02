6df02289ea145757ddb774b06b14b68b56d94927
bkharbech <kharbeche.ibrahim@gmail.com>
2020-05-02 02:09:15 +0200

    initial commit

---
+++ MavenCIDemo/src/main/java/de/conet/recruiting/MavenCIDemo/MavenCiDemoApplication.java
@@ -0,0 +1,13 @@
+package de.conet.recruiting.MavenCIDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenCiDemoApplication.class, args);
+	}
+
+}
---
+++ MavenCIDemo/src/test/java/de/conet/recruiting/MavenCIDemo/MavenCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.conet.recruiting.MavenCIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenCiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
